753756288fe2b93deaa9947567007e8f7b94d04f
Kinga Szac <code1@onet.pl>
2017-08-23 00:16:10 +0200

    New task

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+//Task - Mod 10.3
+$(function(){
+    var carouselList = $('#carousel ul'); //pobieramy listę elementów
+    setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to change the slide
+    function changeSlide() {  //odpowiada za zmianę slajdów
+    	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);  //parametry: 1. dobrać aby nie było przeskoków, 2.czas animacji
+    }
+
+function moveFirstSlide() {  //wykonuje się na końcu animacji (callback) - pierwszy slajd wskoczy za ostatni
+	carouselList.find('li:last')  //metoda find - znajdzie na liście ostatni element
+	.after(carouselList.find('li:first'));  //metoda after i find
+	carouselList.css({marginLeft:0});  //metoda css
+};
+});
