69ab0cc4c1b903c3c7246b08e4b1d075af83ced3
Kinga Szac <code1@onet.pl>
2017-08-26 09:18:12 +0200

    Added changes in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // scripts.js
 
 //Task - Mod 10.3
-$(function(){
+$(function() {
     var carouselList = $('#carousel ul'); //pobieramy listę elementów
     setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to change the slide
     function changeSlide() {  //odpowiada za zmianę slajdów
@@ -10,7 +10,7 @@ $(function(){
 
 function moveFirstSlide() {  //wykonuje się na końcu animacji (callback) - pierwszy slajd wskoczy za ostatni
 	carouselList.find('li:last')  //metoda find - znajdzie na liście ostatni element
-	.after(carouselList.find('li:first'));  //metoda after i find
+		.after(carouselList.find('li:first'));  //metoda after i find
 	carouselList.css({marginLeft:0});  //metoda css
 };
 });
